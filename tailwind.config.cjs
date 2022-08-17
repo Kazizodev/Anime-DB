807ca63ab5fb8c73b6985ef5ffca8ce649d9a1fa
Kazizo <kazizohajar55@gmail.com>
2022-08-17 03:43:57 +0300

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'fira': ['Fira Sans', 'Arial', 'sans-serif']
+    }
+  },
+  plugins: [],
+}
